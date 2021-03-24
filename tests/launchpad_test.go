20a8cbb48586aae197ff4ef1255937d06466a802
enpolat <enpolat@microsoft.com>
2021-03-24 18:46:44 +0300

    TestLaunchpadResourceGroupIsExists test added

--- tests/launchpad_test.go
+++ tests/launchpad_test.go
@@ -1,14 +1,25 @@
 package caf_tests
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLaunchpadResources(t *testing.T) {
+func TestLaunchpadResourceGroupIsExists(t *testing.T) {
+	t.Parallel()
+
+	resourceGroupName := os.Getenv("RESOURCE_GROUP_NAME")
+	subscriptionId := os.Getenv("ARM_SUBSCRIPTION_ID")
+
+	exists := azure.ResourceGroupExists(t, resourceGroupName, subscriptionId)
+
+	assert.True(t, exists, "Resource group does not exist")
+}
 	t.Parallel()
 
 	fmt.Println(os.Getenv("TEST"))
