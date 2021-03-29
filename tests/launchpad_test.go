6fcb657175552fbc2d21fc2d41bc16bc83e73cce
enpolat <enpolat@microsoft.com>
2021-03-29 15:51:14 +0300

    prepareTestData structure moved to utils

--- tests/launchpad_test.go
+++ tests/launchpad_test.go
@@ -3,50 +3,11 @@ package caf_tests
 import (
 	"context"
 	"fmt"
-	"os"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/stretchr/testify/assert"
 )
-
-type LandingZone struct {
-	Level              int
-	ResourceGroupName  string
-	KeyVaultName       string
-	StorageAccountName string
-}
-
-type TestStructure struct {
-	Environment    string
-	Prefix         string
-	SubscriptionID string
-	LandingZones   []LandingZone
-}
-
-// Data-Driven Testing approach implemented
-// https://en.wikipedia.org/wiki/Data-driven_testing
-func prepareTestTable() TestStructure {
-	prefix := os.Getenv("PREFIX")
-
-	test := TestStructure{
-		Prefix:         prefix,
-		SubscriptionID: os.Getenv("ARM_SUBSCRIPTION_ID"),
-		Environment:    os.Getenv("ENVIRONMENT"),
-		LandingZones:   make([]LandingZone, 0),
-	}
-
-	for iLoop := 0; iLoop < 4; iLoop++ {
-		test.LandingZones = append(test.LandingZones, LandingZone{
-			Level:              iLoop,
-			ResourceGroupName:  fmt.Sprintf("%s-rg-launchpad-level%d", prefix, iLoop),
-			KeyVaultName:       fmt.Sprintf("%s-kv-level%d", prefix, iLoop),
-			StorageAccountName: fmt.Sprintf("%sstlevel%d", prefix, iLoop),
-		})
-	}
-
-	return test
-}
 
 func TestLaunchpadResourceGroupIsExists(t *testing.T) {
 	t.Parallel()
