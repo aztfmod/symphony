d50105d6510126ccee30f4b06ff81f7af46bf140
enpolat <enpolat@microsoft.com>
2021-03-30 12:19:06 +0300

    check for value of environment tag added

--- tests/level1_test.go
+++ tests/level1_test.go
@@ -19,7 +19,14 @@ func TestFoundationResourceGroupsDoesNotExist(t *testing.T) {
 
 	rgList := result.Values()
 
+	actual := 0
+	for _, rg := range rgList {
+		if *rg.Tags["environment"] == test.Environment {
+			actual++
+		}
+	}
+
 	expected := 1
 
-	assert.Equal(t, expected, len(rgList), "Resource Group count does not match")
+	assert.Equal(t, expected, actual, "Resource Group count does not match")
 }
