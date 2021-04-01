a4e302631a59d25a6e1c995960d2b3fb4d4bd34e
enpolat <enpolat@microsoft.com>
2021-04-01 17:56:12 +0300

    test added to check if there is networking hub resource groups

--- tests/level3_test.go
+++ tests/level3_test.go
@@ -8,3 +8,26 @@ import (
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestThereAreTwoResourceGroupsForNetworkingHub(t *testing.T) {
+	t.Parallel()
+
+	test := prepareTestTable()
+
+	client, _ := azure.GetResourceGroupClientE(test.SubscriptionID)
+
+	result, _ := client.List(context.Background(), "tagName eq 'level' and tagValue eq 'level2'", nil)
+
+	rgList := result.Values()
+
+	actual := 0
+	for _, rg := range rgList {
+		if *rg.Tags["landingzone"] == "networking_hub" && *rg.Tags["environment"] == test.Environment {
+			actual++
+		}
+	}
+
+	expected := 2
+
+	assert.Equal(t, expected, actual, fmt.Sprintf("There must be %d resource group with 'landingzone=networking_hub' and 'environment=%s' tags, found %d", expected, test.Environment, actual))
+}
