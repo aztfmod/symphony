3e1dff67854dd98b8396b1330ad6d2268c4a9745
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2021-04-05 14:47:27 -0700

    Added changes to support loading msi's by id from symphony.yml

--- scripts/gitlab-server/install.sh
+++ scripts/gitlab-server/install.sh
@@ -6,11 +6,19 @@
 # values needed from server vm
 declare RESOURCE_GROUP="server_group"
 declare GITLAB_TOKEN="<token>"
-declare GITLAB_URL="rguthrie-gitlab-ce.eastus.cloudapp.azure.com"
+declare GITLAB_URL="<gitlab server url>"
 declare CERT_PATH=~/projects/caf/symphony/.data/gitlab.crt
 declare SERVER_INTERNAL_IP="10.0.1.4"
 
 # invoke runner script
-./gitlab-runner-setup.sh -g $RESOURCE_GROUP -d -c $CERT_PATH -gt $GITLAB_TOKEN -gd $GITLAB_URL -si $SERVER_INTERNAL_IP -f
+./gitlab-runner-setup.sh \
+    -g $RESOURCE_GROUP \
+    -c $CERT_PATH \
+    -gt $GITLAB_TOKEN \
+    -gd $GITLAB_URL \
+    -si $SERVER_INTERNAL_IP \
+    -cp ../../symphony.yml \
+    -f \
+    -d \
 
 
