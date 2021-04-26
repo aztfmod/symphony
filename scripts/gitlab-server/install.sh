248b3bb9dc437608243664dde4dc95298e3a0768
Dave Seepersad <davesee@microsoft.com>
2021-04-26 15:40:50 +0000

    Updated install cert path for devcontianer

--- scripts/gitlab-server/install.sh
+++ scripts/gitlab-server/install.sh
@@ -7,19 +7,19 @@
 declare RESOURCE_GROUP="<server_resource_group>"
 declare GITLAB_TOKEN="<gitlab_server_token>"
 declare GITLAB_URL="<gitlab_server_url>"
-declare CERT_PATH=~/projects/caf/symphony/.data/gitlab.crt
+declare CERT_PATH=/workspaces/symphony/.data/ssl/server.crt
 declare SERVER_INTERNAL_IP="10.0.0.4"
 declare ENVIRONMENT="<caf_env>"
 declare CONFIG_PATH="../../symphony.yml"
 
 # invoke runner script
 ./gitlab-runner-setup.sh \
-    -g  $RESOURCE_GROUP \
-    -gt $GITLAB_TOKEN \
-    -gd $GITLAB_URL \
-    -c  $CERT_PATH \
-    -si $SERVER_INTERNAL_IP \
-    -e  $ENVIRONMENT \
-    -cp $CONFIG_PATH \
-    -d  \
-    -f
+  -g $RESOURCE_GROUP \
+  -gt $GITLAB_TOKEN \
+  -gd $GITLAB_URL \
+  -c $CERT_PATH \
+  -si $SERVER_INTERNAL_IP \
+  -e $ENVIRONMENT \
+  -cp $CONFIG_PATH \
+  -d \
+  -f
