3f0d1df5c9a573c53730649ea6c5c2db09db97ff
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2021-05-20 14:41:04 -0700

    removing duplicate function

--- tests/level1_test.go
+++ tests/level1_test.go
@@ -43,15 +43,3 @@ func TestFoundationGlobalSettingsEnvironment(t *testing.T) {
 	//assert
 	assert.Equal(t, tfState.Environment, global_settings["environment"].(string))
 }
-
-func TestFoundationGlobalSettingsEnvironment(t *testing.T) {
-	//arrange
-	t.Parallel()
-	tfState := NewTerraformState(t, "caf_foundations")
-
-	//act
-	global_settings := tfState.GetGlobalSettings()
-
-	//assert
-	assert.Equal(t, tfState.Environment, global_settings["environment"].(string))
-}
